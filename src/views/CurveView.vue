9c4fcf8926eaffed183b41359f1496a00eda481d
kingxi <1010443315@qq.com>
2022-08-26 22:26:22 +0800

    CurveView

--- src/views/CurveView.vue
+++ src/views/CurveView.vue
@@ -3,9 +3,13 @@
 
    <el-container>
      <el-header>
-       曲线
+                                           曲线
      </el-header>
      <el-main>
+       <el-row>
+         <el-button>数据集柱状图</el-button>
+         <el-button type="primary">散点图</el-button>
+       </el-row>
        <template>
          <el-select v-model="value" clearable placeholder="请选择">
            <el-option
@@ -19,7 +23,21 @@
        <template>
          <v-chart class="chart" :option="option" />
        </template>
+
      </el-main>
+     <el-footer>
+       <el-select v-model="value" clearable placeholder="请选择">
+         <el-option
+             v-for="item in options"
+             :key="item.value"
+             :label="item.label"
+             :value="item.value">
+         </el-option>
+       </el-select>
+       <template>
+         <v-chart class="chart" :option="option2" />
+       </template>
+     </el-footer>
    </el-container>
  </div>
 </template>
@@ -32,12 +50,12 @@ export default {
   provide: {
     [THEME_KEY]: "dark"
   },
-  data(){
+  data() {
     return {
       selected: '',
       components: {
-        VChart},
-
+        VChart
+      },
       options: [{
         value: '选项1',
         label: '样品号'
@@ -56,30 +74,61 @@ export default {
       }],
       value: '',
       option: {
-        legend: {orient: 'horizontal',x:'center', y:'top', itemGap: 40},
+        backgroundColor: 'white',
+        legend: {orient: 'horizontal', x: 'center', y: 'top', itemGap: 40},
         tooltip: {},
         dataset: {
           source: [
-            ['product', '黏土基质', '石英粉砂', '空洞','砂或岩屑','其他矿物'],
-            ['HXJFM1', 62.3, 21.7, 12.7,1.3,2],
-            ['HXJFM2', 60.7, 21.7, 17,NaN,0.7],
-            ['HXJFM3', 60.3, 20.3, 18.7,NaN,0.7],
-            ['HXJFM4', 70, 19, 9.3,NaN,1.7],
+            ['product', '黏土基质', '石英粉砂', '空洞', '砂或岩屑', '其他矿物'],
+            ['HXJFM1', 62.3, 21.7, 12.7, 1.3, 2],
+            ['HXJFM2', 60.7, 21.7, 17, NaN, 0.7],
+            ['HXJFM3', 60.3, 20.3, 18.7, NaN, 0.7],
+            ['HXJFM4', 70, 19, 9.3, NaN, 1.7],
           ]
         },
-        xAxis: { type: 'category' },
+        xAxis: {type: 'category'},
         yAxis: {},
         // Declare several bar series, each will be mapped
         // to a column of dataset.source by default.
-        series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' },{ type: 'bar' },{ type: 'bar' }]
-      }
-    };
+        series: [{type: 'bar'}, {type: 'bar'}, {type: 'bar'}, {type: 'bar'}, {type: 'bar'}]
+      },
+      option2: {
+        backgroundColor: 'white',
+        xAxis: {name: '黏土基质'},
 
-  },
-};
+        yAxis: {name: '石英粉砂'},
+        series: [{
+          symbolSize: 20,
+          data: [
+            [34.7, 7.3],
+            [31.8, 6.4],
+            [34.2, 8.2],
+          ],
+          type: 'scatter'
+        }
+        ]
+      },
+    }
+  }
+}
 </script>
+
 <style >
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+.el-footer,.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
 .chart {
   height: 400px;
 }
+
+
 </style>
